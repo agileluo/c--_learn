dae484f1ecb630204e97f6f228c63985f022a925
agileluo <363825970@qq.com>
2013-06-20 16:02:04 +0800

    first commit

---
+++ chapter2/myfirst.cpp
@@ -0,0 +1,16 @@
+/*
+ * myfirst.cpp
+ *
+ *  Created on: 2013-6-20
+ *      Author: v_mliangluo
+ */
+#include <iostream>
+
+int main(){
+	using namespace std;
+	cout << "Fo llow me to learning c++" << endl;
+	cout << "You won't regret it! Yes?" << endl;
+	return 0;
+}
+
+
